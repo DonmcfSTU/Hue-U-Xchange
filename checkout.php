d3ca937eb4d158d1b040b9b701e39b6485f2be90
Donavan McFadden <donmcf8357@students.ecpi.edu>
2025-09-25 21:55:57 -0400

    Create checkout.php

---
+++ checkout.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Checkout - Hue U Xchange</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <header>
+    <nav>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="offerings.php">Offerings</a></li>
+        <li><a href="cart.php">Cart</a></li>
+        <li><a href="about.html">About</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section class="intro">
+      <h1>Initiation Form</h1>
+      <p class="tagline">You are one step away from becoming a Certified Light Carrier.</p>
+
+      <form action="confi
